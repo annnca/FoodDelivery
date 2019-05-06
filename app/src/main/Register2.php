bb15aa2827a144a71fd2d29db212ffc7f3b3ad75
Anca Stanasel <stanasel.anca@yahoo.com>
2019-05-06 22:31:22 +0300

    Update Register2.php

--- app/src/main/Register2.php
+++ app/src/main/Register2.php
@@ -1,5 +1,5 @@
 <?php
-//library for php password luat de la alcineva 
+
     require("password.php");
 
     $connect = mysqli_connect("my_host", "my_user", "my_password", "my_database");
@@ -11,8 +11,7 @@
 
      function registerUser() {
         global $connect, $name, $email, $username, $password;
-		//create a password hash; with password_hash cu default ca algortihm for password
-		//encryption 
+		
         $passwordHash = password_hash($password, PASSWORD_DEFAULT);
         $statement = mysqli_prepare($connect, "INSERT INTO user (name, email, username, password) VALUES (?, ?, ?, ?)");
         mysqli_stmt_bind_param($statement, "siss", $name, $email, $username, $passwordHash);
@@ -22,15 +21,14 @@
 
     function usernameAvailable() {
         global $connect, $username;
-		//selects all users names from db  where the user name
-		//is the one inputted; 
+		 
         $statement = mysqli_prepare($connect, "SELECT * FROM user WHERE username = ?"); 
         mysqli_stmt_bind_param($statement, "s", $username);
         mysqli_stmt_execute($statement);
         mysqli_stmt_store_result($statement);
         $count = mysqli_stmt_num_rows($statement);
         mysqli_stmt_close($statement); 
-		//if it exits....
+		
         if ($count < 1){
             return true; 
         }else {
@@ -40,8 +38,7 @@
 
     $response = array();
     $response["success"] = false;  
-//when the user first registers it checks if the username is available, so 2 people
-//don t have the same username
+
 
     if (usernameAvailable()){
         registerUser();
